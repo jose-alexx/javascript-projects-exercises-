2eb4f9176179fe6df5ce2817f2b3f47930ba89a0
jose-alexx <josealex41012@gmail.com>
2024-07-28 04:37:53 -0300

    atribuindo-novos-projetos
    Fui adicionado novos arquivos do projeto do numero secreto do curso "Lógica de programação: mergulhe em programação com JavaScript" da ALURA

---
+++ Lógica de programação - mergulhe em programação com JavaScript/Desafio-hora-da-pratica-02.js
@@ -0,0 +1,57 @@
+/*
+let diaDaSemana = (prompt("Qual o dia da semana?"));
+
+if (diaDaSemana == "Sabado" || diaDaSemana == "Domingo"){
+  alert("Bom fim de semana!");
+  } 
+else {
+  alert("Boa semana!");
+}
+*/
+
+/*
+let numeroDoUsuario = (prompt("Digite um numero"));
+
+if (numeroDoUsuario > 0) {
+  alert("O numero é positivo");
+}
+else if (numeroDoUsuario < 0) {
+  alert("O numero é negativo");
+}
+else {
+  alert("O numero é zero");
+}
+*/
+
+/*
+let pontuacaodoUsuario = (prompt("Digite a sua pontuação"));
+
+if (pontuacaodoUsuario >= 100) {
+  alert("Parabéns, você venceu!");
+} else {
+  alert("Tente novamente para ganhar");
+}
+*/
+
+/*
+let saldo = (prompt("Digite o seu saldo"));
+
+alert(`O seu saldo é: ${saldo}`);
+*/
+
+let seuNome = (prompt("Digite os eu nome"));
+
+alert("Seja bem vindo(a) " + seuNome);
+
+/*
+Desafios
+Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+
+Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+*/
---
+++ Lógica de programação - mergulhe em programação com JavaScript/Desafio-hora-da-pratica-03.js
@@ -0,0 +1,48 @@
+/*
+let contador = 1;
+
+while (contador <= 10) {
+  console.log(`${contador}, `);
+  contador++;
+}
+*/
+
+/*
+let contador = 10;
+
+while (contador >= 0) {
+  console.log(`${contador}, `);
+  contador--;
+}
+*/
+
+/*
+let numero = (prompt("Digite um numero"));
+
+while (numero >= 0) {
+  console.log(numero + ", ");
+  numero--;
+}
+*/
+
+let numero = (prompt("Digite um numero"));
+let contador = 0;
+
+while (contador <= numero) {
+  console.log(contador + ", ");
+  contador++;
+}
+
+/*
+
+Desafios
+Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+*/
+
